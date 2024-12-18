545e7b9f55133ff3b19335f90a7a7d5c5f38a2a8
SanketPatil20 <sanketvp01@gmail.com>
2024-12-18 23:37:26 +0530

    Initial commit with arrays

---
+++ src/Arrays/SearchElement.java
@@ -0,0 +1,25 @@
+package Arrays;
+
+public class SearchElement {
+    private static void check(int[] arr, int toCheckValue)
+    {
+        // Linear Search
+        boolean test = false;
+        for (int element : arr) {
+            if (element == toCheckValue) {
+                test = true;
+                break;
+            }
+        }
+
+        System.out.println("Is " + toCheckValue
+                + " present in the array: " + test);
+    }
+
+    public static void main(String[] args) {
+        int a[] = {1, 2, 3, 4, 5, 6};
+        int ele = 5;
+
+        check(a,ele);
+    }
+}
