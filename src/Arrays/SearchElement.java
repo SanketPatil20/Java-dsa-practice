40d89611ea281b51cd9f210ab7560014ac801a3f
SanketPatil20 <sanketvp01@gmail.com>
2024-12-18 23:38:12 +0530

    Initial commit with arrays

--- src/Arrays/SearchElement.java
+++ src/Arrays/SearchElement.java
@@ -17,7 +17,7 @@ public class SearchElement {
     }
 
     public static void main(String[] args) {
-        int a[] = {1, 2, 3, 4, 5, 6};
+        int[] a = {1, 2, 3, 4, 5, 6};
         int ele = 5;
 
         check(a,ele);
