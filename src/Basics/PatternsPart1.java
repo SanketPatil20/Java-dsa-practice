6ea3256317a27e89f0ae154741997cad56d5482c
SanketPatil20 <sanketvp01@gmail.com>
2024-12-21 00:00:20 +0530

    first half of patterns

--- src/Basics/PatternsPart1.java
+++ src/Basics/PatternsPart1.java
@@ -61,9 +61,73 @@ public class PatternsPart1 {
         System.out.println(" ");
     }
 
+    public static void pattern7(int n){
+        for (int i=1; i<=n; i++){
+            for (int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2*i-1 ; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        System.out.println(" ");
+    }
 
+    public static void pattern8(int n){
+        for (int i=0; i<n; i++){
+            for (int j=1;j<=i;j++){
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2*n-(2*i+1) ; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        System.out.println(" ");
+    }
 
+    public static void pattern9(int n){
+        //Pattern7 code as it is combined with pattern8 called from function
+        for (int i=1; i<=n; i++){
+            for (int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2*i-1 ; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        pattern8(n);
+        System.out.println(" ");
+    }
 
+    public static void pattern10(int n){
+        for (int i =1; i <= 2*n-1; i++){
+            int star = i;
+            if (i>n) star = 2*n-i;
+
+            for (int j=1;j<=star;j++){
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+        System.out.println(" ");
+    }
+
+    public static void pattern11(int n){
+        int start = 1;
+        for (int i=0; i<n; i++){
+            if (i%2==0) start=1;
+            else start=0;
+            for (int j=0;j<=i;j++){
+                System.out.print(start+" ");
+                start = 1-start;
+            }
+            System.out.println("");
+        }
+        System.out.println(" ");
+    }
 
     public static void main(String[] args) {
         int n = 5;
@@ -73,6 +137,11 @@ public class PatternsPart1 {
         pattern4(n);
         pattern5(n);
         pattern6(n);
+        pattern7(n);
+        pattern8(n);
+        pattern9(n);
+        pattern10(n);
+        pattern11(n);
     }
 
 }
