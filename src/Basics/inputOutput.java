c1abe7f40b617cec8b1a7eb458cd551f78c1aa12
SanketPatil20 <sanketvp01@gmail.com>
2025-06-10 19:02:59 +0530

    check

---
+++ src/Basics/inputOutput.java
@@ -0,0 +1,13 @@
+package Basics;
+
+public class inputOutput {
+    public static int multiplication(int A, int B) {
+        return A * B;
+    }
+
+    public static void main(String[] args) {
+        // Test the function
+        int result = multiplication(5, 7);
+        System.out.println("Multiplication result: " + result);
+    }
+}
