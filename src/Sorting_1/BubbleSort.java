cf16e72b92ae02dd258939d282452fc6931c9908
SanketPatil20 <sanketvp01@gmail.com>
2025-07-07 18:45:29 +0530

    Bubble sort

--- src/Sorting_1/BubbleSort.java
+++ src/Sorting_1/BubbleSort.java
@@ -10,12 +10,19 @@ public class BubbleSort {
 
         for (int i = n-1; i>=0 ; i--) {
 
+            int didSwap = 0; //Optimization for the already sorted array input
+
             for(int j = 0; j<= i-1; j++){ //we are taking j<=i-1 bcz in the next step we are anyways accessing j+1 element which may cause error for the last element of the array
                 if(arr[j] > arr[j+1]){
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
+
+                    didSwap = 1;
                 }
+            }
+            if (didSwap == 0){
+                break;
             }
         }
         System.out.println(Arrays.toString(arr));
